de350134cceca137d827df50502444d775c20bac
3WAPierre93 <pierre.allileche@3wa.io>
2023-12-14 17:15:29 +0100

    Animator Crée mise en place des states

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.XR;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+
+    internal class Jump
+    {
+        #region publicForJump
+        public CharacterController controller;
+
+        Vector3 movedirection;
+
+        public float gravity = -20f;
+
+        public float jumpforce = 8f;
+        #endregion
+        public void Update()
+
+        {
+            // CODE POUR SAUTER 
+            if (Input.GetButtonDown("Jump"))
+          {
+            movedirection.y = jumpforce = 8f;
+
+            Debug.Log("Jump true");
+          }
+          // ADD GRAVITY 
+          movedirection.y += gravity * Time.deltaTime;
+          controller.Move(movedirection * Time.deltaTime);
+
+
+        }
+    
+    }
+
+}
--- Assets/Scripts/TPSMovement.cs
+++ Assets/Scripts/TPSMovement.cs
@@ -1,56 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TPSMovement : MonoBehaviour
 {
+    #region Public
     public CharacterController controller;
     Vector3 movedirection;
 
     public float speed = 5f;
 
-    public float turnSmoothTime = 0.1f;
+    public float runspeed = 10f;
+
+    public float jumpforce = 10f;
 
     public float gravity = -20f;
 
-    public float jumpspeed = 6f;
+    public float stealthmove = 2f;
 
-    public Transform cam; 
+    public float turnSmoothTime = 0.1f;
 
+    public Transform cam;
+   
     float turnSmoothVelocity;
+    #endregion
 
 
-
-    // Update is called once per frame
+    #region Script déplacements
+    // Update is called once per frame// \\ Script pour le déplacement et les mouvements et suivis de caméra\\
     void Update()
     {
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+       
 
 
-        if(direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,turnSmoothTime);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
-            transform.rotation = Quaternion.Euler( 0f, angle ,0f );
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-
-        if(Input.GetButtonDown("Jump"))
+        if (controller.isGrounded)
         {
-            movedirection.y = jumpspeed;
+            movedirection = transform.forward * speed * vertical;
+            movedirection = transform.forward * speed * horizontal;
         }
+        #endregion
 
+        // CODE POUR SAUTER 
+        if (Input.GetButtonDown("Jump"))
+        {
+            movedirection.y = jumpforce = 8f;
+
+            Debug.Log("Jump true");
+        }
         // ADD GRAVITY 
         movedirection.y += gravity * Time.deltaTime;
         controller.Move(movedirection * Time.deltaTime);
+        // Pour Sprinter 
+        if (Input.GetButton("Sprint"))
+        {
 
+            movedirection = transform.forward * runspeed * vertical;
+            movedirection = transform.forward * runspeed * horizontal;
 
-        
+            Debug.Log("Sprint enable");
+        }
+
     }
+
+
 }
+
+
+
+
+
+
+
+
